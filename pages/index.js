72fb4e74c04e0928b672d47eb89008afe6bf3cf2
manmanmeer <masak6161@gmail.com>
2023-10-21 13:10:39 +0900

    title追加

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import styles from '@/styles/Home.module.scss'
-import { client } from "../libs/client";
+import Head from 'next/head';
+import styles from '@/styles/Home.module.scss';
+import { client } from '../libs/client';
 import Link from 'next/link';
 
-//SSG
-export const getStaticProps = async() => {
-  const data = await client.get({ endpoint:"blog" });
+export const getStaticProps = async () => {
+  const data = await client.get({ endpoint: 'blog' });
   return {
     props: {
       blog: data.contents,
@@ -12,16 +12,36 @@ export const getStaticProps = async() => {
   };
 };
 
-export default function Home({ blog }) {
-  return <div className={styles.container}>
-    {blog.map((blog) => (
-      <li key={blog.id}>
-        <Link href={`blog/${blog.id}`}>
-          {blog.title}
-        </Link>
-      </li>
-    ))
-    }
-  </div>; 
-    
-}
+const Home = ({ blog }) => {
+  const featuredBlog = {
+    id: 'your-blog-id',
+    title: '世間的には40歳だけど独学でエンジニアとしてやっていく',
+    publishedAt: new Date().toISOString(),
+  };
+
+  return (
+    <div className={styles.container}>
+      {/* ページ全体のタイトル */}
+      <Head>
+        <title>世間的には40歳だけど独学でエンジニアとしてやっていく</title>
+      </Head>
+
+      {/* ブログ自体のタイトルヘッダー */}
+      <h1 className={styles.maintitle}>{featuredBlog.title}</h1>
+
+      {/* 既存のブログカード */}
+      {blog.map((blog) => (
+        <div key={blog.id} className={styles.blogCard}>
+          <Link href={`blog/${blog.id}`}>
+            <div className={styles.title}>{blog.title}</div>
+          </Link>
+          <p className={styles.publishedAt}>公開日: {blog.publishedAt}</p>
+        </div>
+      ))}
+
+      
+    </div>
+  );
+};
+
+export default Home;
